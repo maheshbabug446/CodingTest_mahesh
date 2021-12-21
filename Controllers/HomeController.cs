7d6c233a5c4aface0544b40d89e2d6b2d498f6d6
mahes <mahes@LAPTOP-30LQC5PD>
2021-12-21 17:30:10 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CodingTests.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nancy.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingTests.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IWebHostEnvironment Environment;
+
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger, IWebHostEnvironment _environment)
+		{
+			_logger = logger;
+			Environment = _environment;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public  IActionResult Index(EmployeeDetails empdetails)
+		{
+			string wwwPath = this.Environment.WebRootPath;
+			wwwPath = wwwPath+ "\\Submissionfile.json";
+			string json = new JavaScriptSerializer().Serialize(empdetails);
+			System.IO.File.WriteAllText(wwwPath, json);
+			return View(empdetails);
+		}
+	}
+}
---
+++ Models/EmployeeDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingTests.Models
+{
+	public class EmployeeDetails
+	{
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+	}
+}
